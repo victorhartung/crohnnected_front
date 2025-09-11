8a7c0fd0de6e1fbfbf3b6f1b2d707a66c3a570eb
victorhartung <victor_hartung@hotmial.com>
2025-09-10 21:48:24 -0300

    delete next

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"app\\\\map\\\\page.tsx -> @/components/map-view\":{\"id\":\"app\\\\map\\\\page.tsx -> @/components/map-view\",\"files\":[\"static/css/_app-pages-browser_components_map-view_tsx.css\",\"static/chunks/_app-pages-browser_components_map-view_tsx.js\"]}}"
